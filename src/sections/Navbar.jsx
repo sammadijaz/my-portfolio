b9e2ea1f573fd3e0ca9138bdf9797e832a639a9a
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-16 00:16:23 +0500

    nav links

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -6,9 +6,17 @@ function Navbar() {
   return (
     <nav 
     ref={navRef} 
-    className='
-    fixed z-50 flex flex-col justify-between w-full h-full px-10 uppercase bg-black text-white/80 py-28 gap-y-10 md:w-1/2 md:left-1/2
-    '>
+    className='fixed z-50 flex flex-col justify-between w-full h-full px-10 uppercase bg-black text-white/80 py-28 gap-y-10 md:w-1/2 md:left-1/2'
+    >
+        <div className='flex flex-col text-5xl gap-y-2 md:text-6xl lg:text-8xl'
+        >
+            {["home","services", "about", "work", "contact"].map((section, index) => (
+                <div>
+                    <a href="">{section}</a>
+                </div>
+            )
+            )}
+        </div>
 
     </nav>
   );
