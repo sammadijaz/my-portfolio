0056c7725eba7aff3b55970ec1daaa58ece3b63f
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-19 02:16:06 +0500

    section + divs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
 
 const App = () => {
   return (
     <div className='relative w-screen min-h-screen overflow-x-auto'>
       <Navbar />
-      <section id='home' className='min-h-screen bg-green-700'/>
-      <section id='services' className='min-h-screen bg-blue-600'/>
+      <Hero />
     </div>
   )
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import { useRef } from "react";
 
 function Hero() {
+
+  const contextRef = useRef(null);
+  const headerRef = useRef(null);
   return (
-    <div>
-      Hero
-    </div>
+    <section id="home" className="flex flex-col justify-end min-h-screen">
+      <div ref={contextRef} >
+        <div style={{ clipPath: "polygon(0 0, 100% 0%, 100% 100%, 0% 100%)" }}>
+          <div 
+          ref={headerRef} 
+          className="flex flex-col justify-center gap-12 pt-16 sm:gap-16"
+          >
+            <p className="text-sm font-light tracking-[0.5rem] uppercase px-10 text-black">
+              404 No Bugs Found
+            </p>
+            <div className="px-10">
+              <h1 className="flex flex-col flex-wrap gap-12 text-black uppercase text-4xl sm:text-7xl md:text-8xl lg:text-9xl sm:gap-16 md:block font">
+                Sammad Ijaz
+              </h1>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </section>
   );
 }
 
