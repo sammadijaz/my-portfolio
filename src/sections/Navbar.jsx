50f2ecf5d79aa8a2238d6ae71158d5cf34402416
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-19 00:24:55 +0500

    refs for spans

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -6,6 +6,8 @@ function Navbar() {
     const navRef = useRef(null);
     const linksRef = useRef([]);
     const contactRef = useRef(null);
+    const topLineRef = useRef(null);
+    const bottomLineRef = useRef(null);
   return (
     <>
         <nav 
@@ -63,8 +65,8 @@ function Navbar() {
             </div>
         </nav>
         <div className='fixed z-50 flex flex-col items-center justify-center gap-1 transition-all duration-300 bg-black rounded-full cursor-pointer w-14 h-14 md:w-20 md:h-20 top-4 right-10'>
-            <span className='block w-8 h-0.5 bg-white rounded-full origin-center'></span>
-            <span className='block w-8 h-0.5 bg-white rounded-full origin-center'></span>
+            <span ref={topLineRef} className='block w-8 h-0.5 bg-white rounded-full origin-center'></span>
+            <span ref={bottomLineRef} className='block w-8 h-0.5 bg-white rounded-full origin-center'></span>
         </div>
     </>
   );
