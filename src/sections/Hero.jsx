607ba368f03e92fd6d3e69c5acb1421cfe064f30
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-20 01:24:46 +0500

    lighting of 3d model

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,6 +4,7 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { Canvas } from "@react-three/fiber";
 import { Planet } from "../components/Planet";
+import { Environment, Lightformer } from "@react-three/drei";
 
 
 function Hero() {
@@ -61,14 +62,43 @@ function Hero() {
         
       </div>
       <figure
-      className="absolute inset-0 z-50"
+      className="absolute inset-0 -z-50"
       style={{ width: "100vw", height:"100vh" }}
       >
         <Canvas 
         shadows
         camera={{ position: [0, 0, -10], fov: 17.5, near: 1, far: 20 }}
         >
+          <ambientLight intensity={0.5} />
           <Planet />
+          <Environment resolution={256}>
+            <group rotation={[-Math.PI / 3, 4, 1]}>
+              <Lightformer
+              form={"circle"}
+              intensity={2}
+              position={[0, 5, -9]}
+              scale={10}
+              />
+              <Lightformer
+              form={"circle"}
+              intensity={2}
+              position={[0, 3, 1]}
+              scale={10}
+              />
+              <Lightformer
+              form={"circle"}
+              intensity={2}
+              position={[-5, -1, -1]}
+              scale={10}
+              />
+              <Lightformer
+              form={"circle"}
+              intensity={2}
+              position={[10, 1, 0]}
+              scale={16}
+              />
+            </group>
+          </Environment>
         </Canvas>
       </figure>
     </section>
