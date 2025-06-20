a0014f8f6e9ec7730cea7cbe390e3cf67083c41b
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-20 19:16:23 +0500

    Made AnimatedHeaderSection

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,6 +6,7 @@ import { Canvas } from "@react-three/fiber";
 import { Planet } from "../components/Planet";
 import { Environment, Float, Lightformer } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
+import AnimatedHeader from "../components/AnimatedHeader";
 
 
 function Hero() {
@@ -35,34 +36,9 @@ function Hero() {
   }, [])
   return (
     <section id="home" className="flex flex-col justify-end min-h-screen">
-      <div ref={contextRef} >
-        <div 
-        style={{ clipPath: "polygon(0 0, 100% 0%, 100% 100%, 0% 100%)" }}
-        >
-          <div 
-          ref={headerRef} 
-          className="flex flex-col justify-center gap-12 pt-16 sm:gap-16"
-          >
-            <p className="text-sm font-light tracking-[0.5rem] uppercase px-10 text-black">
-              404 No Bugs Found
-            </p>
-            <div className="px-7">
-              <h1 className="flex flex-col flex-wrap gap-12 text-black uppercase text-[45px] banner-text-responsive sm:text-7xl md:text-8xl lg:text-9xl sm:gap-16 md:block">
-                Sammad Ijaz
-              </h1>
-            </div>
-          </div>
-        </div>
-        <div className="relative px-10 text-black">
-          <div className="absolute inset-x-0 border-t-2" />
-          <div className="py-12 sm:py-16 text-end">
-            <AnimatedTextLines text={aboutText} className="font-light uppercase value-text-responsive" />
-  
-          </div>
+      
+      <AnimatedHeader />
 
-        </div>
-        
-      </div>
       <figure
       className="absolute inset-0 -z-50"
       style={{ width: "100vw", height:"100vh" }}
