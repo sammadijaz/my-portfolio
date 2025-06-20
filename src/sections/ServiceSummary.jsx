09d4f882687d940c2241d15ccd43b5885984ac0a
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-20 16:56:04 +0500

    section

--- src/sections/ServiceSummary.jsx
+++ src/sections/ServiceSummary.jsx
@@ -1,8 +1,10 @@
 function ServiceSummary() {
   return (
-    <div>
-      ServiceSummary
-    </div>
+    <section
+    className="mt-20 overflow-hidden font-light leading-snug text-center mb-42 contact-text-responsive"
+    >
+      
+    </section>
   );
 }
 
