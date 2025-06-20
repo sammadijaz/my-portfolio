b283e9b022edaa09e8b24d8a443b667b72354610
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-20 17:18:17 +0500

    styling p and divs

--- src/sections/ServiceSummary.jsx
+++ src/sections/ServiceSummary.jsx
@@ -3,16 +3,14 @@ function ServiceSummary() {
     <section
     className="mt-20 overflow-hidden font-light leading-snug text-center mb-42 contact-text-responsive"
     >
-      <div
-      id="title-service-1"
-      >
+      <div id="title-service-1">
         <p>Architecture</p>
       </div>
       <div
       id="title-service-2"
-      className="flex items-center justify-center gap-3 translate-X-16"
+      className="flex items-center justify-center gap-3 translate-x-16"
       >
-        <p>Development</p>
+        <p className="font-normal">Development</p>
         <div className="w-10 h-1 md:w-32 bg-gold"></div>
         <p>Deployment</p>
       </div>
