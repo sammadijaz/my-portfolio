8508390de816e64896f01fac706578415474a1eb
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-21 01:24:49 +0500

    added About details

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,9 +1,25 @@
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
+
 function About() {
+    const text = `Ex-CEO software developer with expertise in content writing,front-end, back-end development, 
+    project management, video editing, content creation and in making brand identities. 
+    Experienced in leading cross-functional teams🍃`
+
   return (
-    <div>
-      About
-    </div>
-  );
-}
+  <section 
+    id="about" 
+    className="min-h-screen bg-black rounded-b-4xl"
+    >
+        <AnimatedHeaderSection
+            subTitle={"Coding with purpose, Built to scale"}    
+            title={"About"}
+            text={text}
+            textColor={"text-white"}
+            withScrollTriger = {true}
+        />
+       
+    </section>
+
+)}
 
 export default About;
