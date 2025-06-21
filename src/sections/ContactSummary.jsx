b27e6b74f0ab620c48a73468b11685ffe3ae0b6d
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-21 22:46:13 +0500

    animated container

--- src/sections/ContactSummary.jsx
+++ src/sections/ContactSummary.jsx
@@ -1,8 +1,10 @@
-import React, { useRef } from 'react'
-import Marquee from '../components/Marquee'
+import React, { useRef } from "react";
+import Marquee from "../components/Marquee";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const ContactSummary = () => {
-  const containerRef = useRef(null)
+  const containerRef = useRef(null);
   const items = [
     "Innovation",
     "Precision",
@@ -19,31 +21,44 @@ const ContactSummary = () => {
     "contact me",
     "contact me",
   ];
+
+  useGSAP(() => {
+    gsap.to(containerRef.current, {
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      }
+    });
+  }, []);
   return (
     <section
       ref={containerRef}
-      className='flex flex-col items-center justify-between min-h-screen gap-12 mt-16'
+      className="flex flex-col items-center justify-between min-h-screen gap-12 mt-16"
     >
-      <Marquee items={items}/>
+      <Marquee items={items} />
 
-      <div className='overflow-hidden font-light text-center contact-text-responsive'>
+      <div className="overflow-hidden font-light text-center contact-text-responsive">
         <p>
           "Let's build a <br />
-          <span className='font-normal'>memorable</span> &{" "} <span className='italic'>inspiring</span> <br />
-          web application <span className='text-gold'>together</span>"
+          <span className="font-normal">memorable</span> &{" "}
+          <span className="italic">inspiring</span> <br />
+          web application <span className="text-gold">together</span>"
         </p>
       </div>
 
-      <Marquee 
-        items={items2} 
+      <Marquee
+        items={items2}
         reverse={true}
-        className='text-black bg-transparent border-y-2'
-        iconClassName='stroke-gold stroke-2 text-primary'
-        icon='material-symbols-light:square'
+        className="text-black bg-transparent border-y-2"
+        iconClassName="stroke-gold stroke-2 text-primary"
+        icon="material-symbols-light:square"
       />
-
     </section>
-  )
-}
+  );
+};
 
-export default ContactSummary
+export default ContactSummary;
