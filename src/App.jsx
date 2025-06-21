4b338e895d4c890ec55d1c3ddb414bee3f76026c
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-22 00:30:15 +0500

    trying

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
   }, [progress]);
 
   return (
-    <ReactLenis root className="relative w-screen min-h-screen overflow-hidden">
+    <ReactLenis root className="relative w-screen min-h-screen overflow-hidden overflow-x-hidden">
       {!isReady && (
         <div className="fixed inset-0 z-[999] flex flex-col items-center justify-center bg-black text-white tracking-opacity duration-700 font-light">
           <p className="mb-4 text-xl tracking-widest animate-pulse">
