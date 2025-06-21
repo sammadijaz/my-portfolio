bc07588385ca478bb80fcc57b23fc02a3c7000d4
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-22 00:39:31 +0500

    test 5

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
         <Hero />
         <ServiceSummary />
         <Services />
-        {/* <About /> */}
+        <About />
         {/* <Works /> */}
         {/* <ContactSummary /> */}
         {/* <Contact /> */}
