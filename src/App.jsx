6934c9dfe9f0f00a655d0300203b14fc367e3906
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-22 00:36:55 +0500

    test 3

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ const App = () => {
       >
         <Navbar />
         <Hero />
-        {/* <ServiceSummary /> */}
+        <ServiceSummary />
         {/* <Services /> */}
         {/* <About /> */}
         {/* <Works /> */}
