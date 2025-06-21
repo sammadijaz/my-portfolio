5a4944bc26c445ab335e1c5c4b4237268d4f991c
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-22 00:34:52 +0500

    text 1

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Works from "./sections/Works";
 import ContactSummary from "./sections/ContactSummary";
 import Contact from "./sections/Contact";
 import { useProgress } from "@react-three/drei";
-import { div } from "three/tsl";
 
 const App = () => {
   const { progress } = useProgress();
@@ -39,13 +38,13 @@ const App = () => {
         } transition-opacity duration-1000`}
       >
         <Navbar />
-        <Hero />
-        <ServiceSummary />
-        <Services />
-        <About />
-        <Works />
-        <ContactSummary />
-        <Contact />
+        {/* <Hero /> */}
+        {/* <ServiceSummary /> */}
+        {/* <Services /> */}
+        {/* <About /> */}
+        {/* <Works /> */}
+        {/* <ContactSummary /> */}
+        {/* <Contact /> */}
       </div>
     </ReactLenis>
   );
