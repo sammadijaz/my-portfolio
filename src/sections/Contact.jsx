a46b9a7ce6a0c7b32a07457eccd2f2010193d2e7
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-21 23:13:26 +0500

    added contacts and styling

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,6 +1,7 @@
-import AnimatedHeaderSection from "../components/AnimatedHeaderSection"
-
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
+import { socials } from "../constants";
 const Contact = () => {
+  const text = `Got a question or any project idea? I'd love to hear about it and discuss further!`;
   return (
     <section
       id="contact"
@@ -8,15 +9,44 @@ const Contact = () => {
     >
       <div>
         <AnimatedHeaderSection
-        subTitle={"Logic meets Aesthetics, Seamlessly"}
-        title={"Works"}
-        text={text}
-        textColor={"text-black"}
-        withScrollTriger={true}
-      />
+          subTitle={"You Dream it, I code it"}
+          title={"Contact"}
+          text={text}
+          textColor={"text-white"}
+          withScrollTriger={true}
+        />
+        <div className="flex px-10 font-light text-white uppercase lg:text-[32px] text-[26px] leading-none mb-10">
+          <div className="flex flex-col w-full gap-10">
+            <div className="social-link">
+              <h2>E-Mail</h2>
+              <div className="w-full h-px my-2 bg-white/30" />
+              <p className="text-xl tracking-wider lowercase md:text-2xl lg:text-3xl">iamsammadijaz@gmail.com</p>
+            </div>
+            <div className="social-link">
+              <h2>Phone</h2>
+              <div className="w-full h-px my-2 bg-white/30" />
+              <p className="text-xl lowercase md:text-2xl lg:text-3xl py-1.5">
+                +92-300-2612-932
+              </p>
+            </div>
+            <div className="social-link">
+              <h2>Social Media</h2>
+              <div className="w-full h-px my-2 bg-white/30" />
+              <div className="flex flex-wrap gap-2">
+                {socials.map((social, index) => (
+                  <a key={index} href={social.href} className="text-md leading-loose tracking-widest uppercase md:text-2xl  lg:text-3xl hover:text-white/80 transition-colors duration-200">
+                    {"{ "}
+                    {social.name}
+                    {" }"}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
