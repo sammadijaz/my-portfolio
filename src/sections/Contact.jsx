833ffbea832fca52ae77b99442c1de9908708930
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-21 22:51:48 +0500

    div

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,10 +1,20 @@
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection"
+
 const Contact = () => {
   return (
     <section
       id="contact"
       className="flex flex-col justify-between min-h-screen bg-black"
     >
-
+      <div>
+        <AnimatedHeaderSection
+        subTitle={"Logic meets Aesthetics, Seamlessly"}
+        title={"Works"}
+        text={text}
+        textColor={"text-black"}
+        withScrollTriger={true}
+      />
+      </div>
     </section>
   )
 }
