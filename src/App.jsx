10b6a8e6021f54384f774fbdac342d4c48373542
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-21 23:41:24 +0500

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,54 @@
-import React from 'react'
-import Navbar from './sections/Navbar'
-import Hero from './sections/Hero'
-import ServiceSummary from './sections/ServiceSummary'
-import Services from './sections/Services'
-import ReactLenis from 'lenis/react'
-import About from './sections/About'
-import Works from './sections/Works'
-import ContactSummary from './sections/ContactSummary'
-import Contact from './sections/Contact'
+import React, { useEffect, useState } from "react";
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+import ServiceSummary from "./sections/ServiceSummary";
+import Services from "./sections/Services";
+import ReactLenis from "lenis/react";
+import About from "./sections/About";
+import Works from "./sections/Works";
+import ContactSummary from "./sections/ContactSummary";
+import Contact from "./sections/Contact";
+import { useProgress } from "@react-three/drei";
+import { div } from "three/tsl";
 
 const App = () => {
-  return (
-    <ReactLenis root className='relative w-screen min-h-screen overflow-x-auto'>
-      <Navbar />
-      <Hero />
-      <ServiceSummary />
-      <Services />
-      <About />
-      <Works />
-      <ContactSummary />
-      <Contact  />
-    </ReactLenis>
-  )
-}
+  const { progress } = useProgress();
+  const [isReady, setIsReady] = useState(false);
 
-export default App
+  useEffect(() => {
+    if (progress === 100) {
+      setIsReady(true);
+    }
+  }, [progress]);
+
+  return (
+    <ReactLenis root className="relative w-screen min-h-screen overflow-x-auto">
+      {!isReady && (
+        <div className="fixed inset-0 z-[900] flex flex-col items-center justify-center bg-black text-white tracking-opacity duration-700 font-light">
+          <p className="mb-4 text-xl tracking-widest animate-pulse">
+            Loading {Math.floor(progress)}%
+          </p>
+          <div className="relative h-1 overflow-hidden rounded w-60 bg-white/20">
+            <div className="absolute top-0 left-0 h-full transition-all duration-300 bg-white" style={{width: `${progress}%`}}></div>
+          </div>
+        </div>
+      )}
+      <div
+        className={`${
+          isReady ? "opacity-100" : "opacity-0"
+        } transition-opacity duration-1000`}
+      >
+        <Navbar />
+        <Hero />
+        <ServiceSummary />
+        <Services />
+        <About />
+        <Works />
+        <ContactSummary />
+        <Contact />
+      </div>
+    </ReactLenis>
+  );
+};
+
+export default App;
