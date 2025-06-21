e45ffe8725bed6a61927c3a71636b1d6997b9f76
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-21 15:55:53 +0500

    imported AnimatedHeaderSection

--- src/sections/Works.jsx
+++ src/sections/Works.jsx
@@ -1,6 +1,17 @@
+import AnimatedHeaderSection from "../components/AnimatedHeaderSection"
+
 const Works = () => {
+  
   return (
-    <div>Works</div>
+    <section id="work" className="flex flex-col min-h-screen">
+      <AnimatedHeaderSection
+            subTitle={"Coding with purpose, Built to scale"}    
+            title={"About"}
+            text={text}
+            textColor={"text-white"}
+            withScrollTriger = {true}
+        />
+    </section>
   )
 }
 
