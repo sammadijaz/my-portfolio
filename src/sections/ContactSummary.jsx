9ae0f4d8f1b8ec6abb15ac0ef5937133f8f38e9b
Sammad Ijaz <sammadijaz11223@gmail.com>
2025-06-21 21:47:56 +0500

    section + refs & styling

--- src/sections/ContactSummary.jsx
+++ src/sections/ContactSummary.jsx
@@ -1,9 +1,15 @@
-function ContactSummary() {
+import React, { useRef } from 'react'
+
+const ContactSummary = () => {
+  const containerRef = useRef(null)
   return (
-    <div>
-      ContactSummary
-    </div>
-  );
+    <section
+      ref={containerRef}
+      className='flex flex-col items-center justify-between min-h-screen gap-12 mt-16'
+    >
+      
+    </section>
+  )
 }
 
-export default ContactSummary;
+export default ContactSummary
